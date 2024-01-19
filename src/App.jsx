c4d0e9fbc60e3b6bec57c55ad3fd34e6191875be
Baaz-Cheema <chrisbahadur1@gmail.com>
2024-01-20 03:53:30 +0500

    feat: store and router dom implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Root from "./pages/Root";
+import MainPage from './pages/MainPage';
+import './App.css'
 
 
+const router = createBrowserRouter([
+  {
+    path: '/', element: <Root />, children: [
+      { path: 'main', element: <MainPage /> }
+    ]
+  }
+]
+)
 
 function App() {
-
-
   return (
-    <div className="parent">
-      
-    </div>
+    <RouterProvider router={router}></RouterProvider>
   );
 }
 
