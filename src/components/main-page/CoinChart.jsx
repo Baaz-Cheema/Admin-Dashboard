061e1b00bb97991d6aaeb7d55bd7e8ecddecf1e1
Baaz-Cheema <chrisbahadur1@gmail.com>
2024-01-21 03:05:22 +0500

    refactor: make Chart responsive (was hard...)

--- src/components/main-page/CoinChart.jsx
+++ src/components/main-page/CoinChart.jsx
@@ -1,56 +1,104 @@
-import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid, ResponsiveContainer, Legend } from 'recharts';
 import { getTime } from '../../util/getTime';
 import { data } from '../../util/data';
 import FearGreedIndex from './FearGreedIndex';
-const dataa = [
-    { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
-    { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
-    { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
-    { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
-    { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
-    { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
-    { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
-];
+import 'chart.js/auto'; // ADD THIS
+
+import { useEffect, useState } from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
 
+console.log(data.prices.length)
+const hello = data.prices.map((a, i) => (
+    { time: getTime(a[0]), price: a[1] }
+))
+
+
+const CustomTooltip = ({ active, payload }) => {
+    if (active && payload && payload.length) {
+        const time = payload[0].payload.time;
+        const price = payload[0].payload.price;
+        return (
+            <div className="bg-zinc-500 opacity-75 p-3">
+                <p className="label">{`Price : ${price.toFixed(2)}`}</p>
+                <p className="intro">{`Time : ${time}`}</p>
+            </div>
+        );
+    }
+    return null;
+}
+
+const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+        legend: {
+            display: false,
+            position: 'top',
+        },
+    },
+};
 
 export default function CoinChart() {
+    const [loading, setLoading] = useState(true)
+    const labels = data && data.prices.map((a) => getTime(a[0]))
+    const chartdata = {
+        labels,
+        datasets: [
+            {
+                label: 'crypto chart',
+                data: data && data.prices.map((a) => a[1]),
+                borderColor: 'rgb(251 ,191 ,36)',
+                backgroundColor: 'rgb(251 ,191 ,36)',
+
+            },]
+    }
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 1000)
+    }, [])
     return (
-        <main className=' h-[30rem] flex gap-5 mb-20'>
-            <section className='pb-12 border border-zinc-700 font-poppins flex-[.7]'>
-                <div className='flex items-center justify-between px-8'>
-                    <div className='flex items-center gap-3'>
-                        <div className='w-10'>
-                            <img className='w-full h-full object-cover' src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400" alt="" />
+        <main className='flex gap-5 mb-10 lg:flex-col'>
+            {
+                <section className='border border-zinc-700 w-[70%] lg:w-full font-poppins flex-[.7] lg:max-h-none rounded-lg relative'>
+                    <div className='flex items-center justify-between px-8 md:px-3'>
+                        <div className='flex items-center gap-3'>
+                            <div className='w-10 md:py-5'>
+                                <img className='w-full h-full object-cover' src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400" alt="" />
+                            </div>
+                            <h3 className='font-poppins font-bold'>Bitcoin</h3>
                         </div>
-                        <h3 className='font-poppins font-bold'>Bitcoin</h3>
+                        <div className='py-3 flex items-center gap-4 md:hidden'>
+                            <div className='px-5 w-[12rem] py-3 border border-zinc-700 flex items-center justify-between cursor-pointer'>
+                                <button >Data Type</button>
+                                <i className='bx bx-chevron-down' ></i>
+                            </div>
+                            <div className='px-5 py-3 w-[12rem] border border-zinc-700 flex items-center  justify-between'>
+                                <button>Weekly</button>
+                                <i className='bx bx-chevron-down' ></i>
+                            </div>
+                        </div>
                     </div>
-                    <div className='py-3 flex items-center gap-4'>
-                        <div className='px-5 w-[12rem] py-3 border border-zinc-700 flex items-center justify-between'>
-                            <button >Data Type</button>
-                            <i className='bx bx-chevron-down' ></i>
-                        </div>
-                        <div className='px-5 py-3 w-[12rem] border border-zinc-700 flex items-center  justify-between'>
-                            <button>Weekly</button>
-                            <i className='bx bx-chevron-down' ></i>
-                        </div>
+                    {loading ?
+                        <div className='w-full h-[28rem] lg:h-[40rem] flex justify-center items-center pb-10 md:h-[35rem]'>
+                            <i className='bx bx-loader-alt bx-spin text-6xl text-blue-500 ' ></i>
+                        </div> :
+                        hello ?
+                            <div className='h-[28rem] pb-2 px-10 lg:w-full md:px-0 md:pb-2 lg:h-[40rem] md:h-[35rem]' >
+                                <Line options={options} data={chartdata}></Line>
+                            </div> : 'dasas'}
+                </section>}
 
-                    </div>
-                </div>
-
-                <ResponsiveContainer width="100%" height="100%">
-                    <LineChart
-                        data={dataa}
-                    >
-                        <CartesianGrid strokeDasharray="3 3" stroke="darkgray" />
-                        <XAxis dataKey="name" axisLine={false} />
-                        <YAxis axisLine={false} />
-                        <Tooltip />
-                        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 2 }} />
-                        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-                    </LineChart>
-                </ResponsiveContainer>
-            </section>
             <FearGreedIndex />
         </main>
     )
