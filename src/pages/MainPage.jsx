d3c73c371b5493945c9c00a31e046ce80f649334
Baaz-Cheema <chrisbahadur1@gmail.com>
2024-01-22 23:52:38 +0500

    feat: add new control button component

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,21 +1,15 @@
 import CoinChart from "../components/main-page/CoinChart"
 import SearchBar from "../components/main-page/SearchBar"
 import CoinListSummary from "../components/main-page/CoinListSummary"
+import ControlButtons from "../components/main-page/ControlButtons"
 export default function MainPage() {
     return (
-        <section className="py-5 px-10 flex-1 sm:px-1 mb-20">
+        <section className="py-5 px-10 flex-1 sm:px-3 md:mb-20 ">
             <SearchBar />
-            <h3 className='font-semibold mb-1 text-xl'>Main Page</h3>
-            <p className='text-gray-400 mb-5'>Get a snapshot of the current market state.</p>
-            <div className='py-3 flex items-center gap-4'>
-                <div className='px-5 w-1/2 py-3 border border-zinc-700 flex items-center justify-between cursor-pointer'>
-                    <button >Data Type</button>
-                    <i className='bx bx-chevron-down' ></i>
-                </div>
-                <div className='px-5 py-3 w-1/2 border border-zinc-700 flex items-center  justify-between'>
-                    <button>Weekly</button>
-                    <i className='bx bx-chevron-down' ></i>
-                </div>
+            <h3 className='font-semibold font-poppins mb-1 text-xl'>Overview</h3>
+            <p className='text-gray-400 font-poppins mb-5'>Get a snapshot of the current market state.</p>
+            <div className='py-3 flex-1 items-center gap-4 hidden md:flex'>
+                <ControlButtons />
             </div>
             <CoinChart />
             <CoinListSummary />
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,10 +1,13 @@
 import Navbar from "../components/Navbar"
 import { Outlet } from "react-router-dom"
+import SearchBar from "../components/main-page/SearchBar"
 
 
 export default function Root() {
     return <div className="bg-zinc-800 text-zinc-100 flex relative">
+
         <Navbar />
+        
         <Outlet />
     </div>
 }
