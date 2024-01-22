6d404e8edf5991dda25d0b4fca179f2d0acd4037
Baaz-Cheema <chrisbahadur1@gmail.com>
2024-01-22 23:50:26 +0500

    feat: add animations and state logic to search bar

--- src/components/main-page/SearchBar.jsx
+++ src/components/main-page/SearchBar.jsx
@@ -1,5 +1,10 @@
 import { NavLink } from "react-router-dom"
+import { useState } from "react"
+import { motion, AnimatePresence } from "framer-motion"
+import { ClickAwayListener } from "@mui/material"
 export default function SearchBar() {
+    const [open, setOpen] = useState(false)
+    const [menu, setMenu] = useState(false)
     return (
         <main className="mb-10 font-poppins">
             <section className="flex  gap-7  md:hidden">
@@ -16,7 +21,7 @@ export default function SearchBar() {
                 </div>
 
             </section>
-            <div className="flex-1 w-full hidden md:flex items-center justify-between ">
+            <div className="flex-1 w-full hidden md:flex items-center justify-between py-3">
                 <NavLink className="flex items-center gap-2">
                     <div className="w-9">
                         <img src="https://altcoinsbox.com/wp-content/uploads/2023/01/nexa-logo.png" alt="" />
@@ -27,11 +32,27 @@ export default function SearchBar() {
                     </div>
 
                 </NavLink>
-                <div className="text-3xl flex gap-8 items-center">
-                    <i className="fi fi-rr-search text-2xl"></i>
-                    <i className="fi fi-rr-menu-burger"></i>
-                </div>
-            </div>
-        </main>
+                <ClickAwayListener onClickAway={() => setOpen(false)}>
+                    <div className="flex gap-5 items-center relative">
+                        <AnimatePresence mode="wait">
+                            {open ?
+                                <motion.div key={'input'} style={{ transformOrigin: 'right' }} layout animate={{ scaleX: [0, 1], transformOrigin: 'right' }} exit={{ scaleX: 0, opacity: 0, transition: { duration: .2 } }} className="flex items-center max-w-[9rem] flex-1 py-2 gap-5 bg-zinc-800 border border-zinc-700  px-3 rounded">
+                                    <i className="fi fi-rr-search"></i>
+                                    <input className="bg-zinc-800 w-full focus:outline-none" type="text" placeholder="Search..." />
+                                </motion.div>
+                                :
+                                <motion.i key={'icon'} initial={{ opacity: 0, x: -10 }} exit={{ opacity: 0, x: -10 }} animate={{ opacity: 1, x: 10, y: 2 }} transition={{ duration: .1 }} onClick={() => setOpen(!open)} className="bx bx-search text-4xl " ></motion.i>}
+                        </AnimatePresence>
+                        <i onClick={() => setMenu(!menu)} className='bx bx-menu-alt-right text-5xl'></i>
+                        {menu && <motion.div layout className="absolute top-20 bg-green-600 h-fit right-0 bottom-0 w-screen overflow-hidden" initial={{ opacity: 0, height: 0 }} animate={{ opacity: 1, height: 'fit-content' }}>
+                            <li>hello</li>
+                            <li>hello</li>
+                            <li>hello</li>
+                            <li>hello</li>
+                        </motion.div>}
+                    </div>
+                </ClickAwayListener>
+            </div >
+        </main >
     )
 }
